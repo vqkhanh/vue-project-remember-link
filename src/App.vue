377f6967141e8b3dccf20a8486a5d9cdafdfa35b
vqkhanh <vqkhanh2009@gmail.com>
2022-09-24 09:57:56 +0700

    init dummy data

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <ul></ul>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      storeResources: [
+        {
+          id: 'offical guide',
+          title: 'Official Guide',
+          description: 'The official Vue.js documentation.',
+          link: 'https://vuejs.org',
+        },
+        {
+          id: 'google',
+          title: 'Google Guide',
+          description: 'Learning everything',
+          link: 'https://google.com',
+        },
+      ],
+    };
+  },
+};
+</script>
