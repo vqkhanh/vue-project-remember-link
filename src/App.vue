18ba53a8bd5f6339b3fce9f8f01fbe9553d8a47b
vqkhanh <vqkhanh2009@gmail.com>
2022-09-24 12:00:39 +0700

    Play 128. First Components & Props

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,22 @@
 <template>
-  <ul></ul>
+  <ul>
+    <learning-resource
+      v-for="res in storeResources"
+      :key="res.id"
+      :title="res.title"
+      :description="res.description"
+      :link="res.link"
+    ></learning-resource>
+  </ul>
 </template>
 
 <script>
+import LearningResource from './components/learning-resources/LearningResource.vue';
+
 export default {
+  components: {
+    LearningResource,
+  },
   data() {
     return {
       storeResources: [
